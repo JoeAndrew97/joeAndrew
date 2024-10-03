3e0709a1d85244f23d8f285f2a6f45f9e4f75172
JoeAndrew97 <08jandrew@googlemail.com>
2024-10-03 14:10:05 +0100

    Delete project1/backend/config.php

--- project1/backend/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$config = [
-    "API_ACCESS" => [
-        "GEONAMES_USERNAME" => "joeandrew",
-        "OPENWEATHER_API_KEY" => "07b1ea28451dfa3c94d4aba9ad00204c",
-        "OPENEXCHANGERATES_API_KEY" => "298863dbce8b44558812620c670d480e",
-    ]
-];
